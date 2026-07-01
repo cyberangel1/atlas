c0e1070595c450bf6a36d4264fc292026847e68f
Levis Bikenzi <levisbikenzi@Leviss-MacBook-Pro.local>
2026-07-01 01:00:26 -0500

    feature(UI): Added Atlas infra dashboard for user friendly view

--- internal/api/server.go
+++ internal/api/server.go
@@ -2,25 +2,42 @@ package api
 
 import (
 	"encoding/json"
+	"html/template"
 	"net/http"
 
 	"github.com/cyberangel1/atlas/internal/config"
 	"github.com/cyberangel1/atlas/internal/health"
 )
 
+func collect(cfg *config.Config) []health.Status {
+
+	var results []health.Status
+
+	for _, s := range cfg.Services {
+		results = append(results, health.CheckService(s))
+	}
+
+	return results
+}
+
 func Start(cfg *config.Config) {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		t := template.Must(template.ParseFiles("web/templates/index.html"))
+
+		t.Execute(w, collect(cfg))
+
+	})
 
 	http.HandleFunc("/services", func(w http.ResponseWriter, r *http.Request) {
 
-		var results []health.Status
-
-		for _, service := range cfg.Services {
-			results = append(results, health.CheckService(service))
-		}
-
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(results)
+
+		json.NewEncoder(w).Encode(collect(cfg))
+
 	})
 
 	http.ListenAndServe(":8080", nil)
+
 }
